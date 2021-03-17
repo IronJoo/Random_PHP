4e4e9eadb43652e8159da78a6413562a9b17c7bb
IronJoo <joanaferro92@gmail.com>
2021-03-17 16:22:23 +0000

    Questionnaire (bugged)

--- index.php
+++ index.php
@@ -2,27 +2,45 @@
 <html>
 <head>
   <meta charset=utf-8>
-  <title>Madlibs Game</title>
+  <title>Questionnaire</title>
 </head>
 <body>
-    <form method="get" action="index.php">
-        <h2>Please input the following:</h2>
-        Color: <input type="text" name="color"><br><br>
-        Plural Noun: <input type="text" name="pluralNoun"><br><br>
-        Celebrity: <input type="text" name="celebrity"><br><br>
-        <input type="submit" value="Submit Inputs">
+    <form method="post" action="index.php">
+        Are you male? <br>
+        Yes <input type="checkbox" name="male"> <br><br>
+        Are you tall? <br>
+        Yes <input type="checkbox" name="tall"> <br><br>
+        <input type="submit" value="Submit Selection">
     </form>
     <br>
     <?php
-        $color = $_GET["color"];
-        $pluralNoun = $_GET["pluralNoun"];
-        $celebrity = $_GET["celebrity"];
-    
-    
-        echo "Roses are $color <br>";
-        echo "$pluralNoun are blue <br>";
-        echo "I love $celebrity <br>";
-    
+      if($_POST['male'] == true){
+        $male = true;
+      }
+      else {$male = false;}
+
+      if($_POST['tall'] == true){
+        $tall = true;
+      }
+      else{$tall = false;}
+
+      if($male == true && $tall == true)
+      {
+        echo "<h4>You are male and tall!</h4>";
+      }
+      elseif ($male == true && $tall == false)
+      {
+        echo "<h4>You are male and not tall!</h4>";
+      }
+      elseif($male == false && $tall == true)
+      {
+        echo "<h4>You are not male and you are tall!</h4>";
+      }
+      else
+      {
+        echo "<h4>You are neither male nor tall!</h4>";
+      }
+
     ?>
 </body>
 </html>
